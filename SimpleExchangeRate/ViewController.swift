a80a559f23218b359e9cd48cb98bc12b5f474cd3
Daheen Lee <allwhite.dev@gmail.com>
2020-01-02 23:35:25 +0900

    Call getRates() method in view controller and subscribe it

--- SimpleExchangeRate/ViewController.swift
+++ SimpleExchangeRate/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let service = NetworkService()
+        service.getRates(for: .USD).subscribe { (event) in
+            print(event)
+        }
     }
 
 
