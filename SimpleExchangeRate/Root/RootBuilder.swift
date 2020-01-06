333aa8a8dd354c40f2b8a66c1493601a9146b1ba
daheenallwhite <dana.alllwhite@kakaopaycorp.com>
2020-01-06 17:08:40 +0900

    Apply .gitignore

---
+++ SimpleExchangeRate/Root/RootBuilder.swift
@@ -0,0 +1,40 @@
+//
+//  RootBuilder.swift
+//  SimpleExchangeRate
+//
+//  Created by dana.allwhite on 2020/01/06.
+//  Copyright © 2020 allwhite. All rights reserved.
+//
+
+import RIBs
+
+protocol RootDependency: Dependency {
+    // TODO: Declare the set of dependencies required by this RIB, but cannot be
+    // created by this RIB.
+}
+
+final class RootComponent: Component<RootDependency> {
+
+    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol RootBuildable: Buildable {
+    func build() -> RootRouting
+}
+
+final class RootBuilder: Builder<RootDependency>, RootBuildable {
+
+    override init(dependency: RootDependency) {
+        super.init(dependency: dependency)
+    }
+
+    func build() -> RootRouting {
+        let component = RootComponent(dependency: dependency)
+        let viewController = RootViewController()
+        let interactor = RootInteractor(presenter: viewController)
+        
+        return RootRouter(interactor: interactor, viewController: viewController)
+    }
+}
