df6e3d3d7c11e117125c9120173177e818ab8b52
daheenallwhite <dana.alllwhite@kakaopaycorp.com>
2020-01-06 17:16:40 +0900

    Add new initializer and empty class property to ResultData

--- SimpleExchangeRate/Model/ResultData.swift
+++ SimpleExchangeRate/Model/ResultData.swift
@@ -9,6 +9,12 @@
 import Foundation
 
 struct ResultData: Decodable {
+    static let empty = ResultData(
+    base: "USD",
+    date : "2020.01.06",
+    lastUpdatedTime: 100,
+    rates: ["KRW": 1200.0])
+    
     let base: String?
     let date: String?
     let lastUpdatedTime: Int?
@@ -19,6 +25,16 @@ struct ResultData: Decodable {
         case lastUpdatedTime = "time_last_updated"
     }
     
+    init(base: String,
+         date: String,
+         lastUpdatedTime: Int,
+         rates: [String: Double]) {
+        self.base = base
+        self.date = date
+        self.lastUpdatedTime = lastUpdatedTime
+        self.rates = rates
+    }
+    
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         
@@ -26,5 +42,12 @@ struct ResultData: Decodable {
         self.date = try? values.decode(String.self, forKey: .date)
         self.lastUpdatedTime = try? values.decode(Int.self, forKey: .lastUpdatedTime)
         self.rates = try? values.decode([String: Double].self, forKey: .rates)
+    }
+    
+    var ratesString: [String] {
+        guard let rates = self.rates else { return [] }
+        return rates.map({ (key, value) -> String in
+            "\(key) \(value)"
+        })
     }
 }
