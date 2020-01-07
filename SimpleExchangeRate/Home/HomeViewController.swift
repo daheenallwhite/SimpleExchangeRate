641dae86e2ff645181d624803d2cfb8aab0f5a1f
dana.allwhite <dana.alllwhite@kakaopaycorp.com>
2020-01-07 17:24:40 +0900

    Modify HomeViewController to use HomeViewModel

--- SimpleExchangeRate/Home/HomeViewController.swift
+++ SimpleExchangeRate/Home/HomeViewController.swift
@@ -29,28 +29,87 @@ final class HomeViewController: UIViewController, HomePresentable, HomeViewContr
     }
     
     @IBOutlet weak var tableView: UITableView!
+    let pickerView = UIPickerView()
+    
+    private let exchangeRateAPI = ExchangeRateAPI.shared
+    private let allCurrencyCodes = CurrencyCode.allCases
+    private let bag = DisposeBag()
+    private let viewModel = HomeViewModel()
        
-       private let bag = DisposeBag()
-       
-       override func viewDidLoad() {
-           super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-           let service = NetworkService()
-           
-           let rates = service.getRates(for: .USD)
-               .map { result -> [String] in
-                   print(result)
-                   guard case .success(let value) = result else {
-                       return []
-                   }
-                   return value.ratesString
-               }
-               .share(replay: 1)
-               .observeOn(MainScheduler.instance)
-           
-           rates.bind(to: tableView.rx.items(cellIdentifier: "Cell")) { row, model, cell in
-               cell.textLabel?.text = model
-           }.disposed(by: bag)
-           
-       }
+        addCurrencyPickerOnTitle()
+        
+        pickerView.rx.modelSelected(CurrencyCode.self)
+            .map { (codes) -> CurrencyCode in
+                return codes[0]
+            }.bind(to: viewModel.baseCurrencySelected)
+            .disposed(by: bag)
+            
+        
+        viewModel.selectedCurrencyRates
+            .drive(tableView.rx.items(cellIdentifier: "Cell")){ row, model, cell in
+            cell.detailTextLabel?.text = model.rate
+            cell.textLabel?.text = model.code
+            
+        }.disposed(by: bag)
+        
+        viewModel.lastUpdatedTime
+            .drive(onNext: { (string) in
+                self.navigationItem.prompt = string
+            }).disposed(by: bag)
+       
+//       let result = exchangeRateAPI.getRates(for: .USD)
+//           .map { result -> ResultData in
+//               print(result)
+//               guard case .success(let value) = result else {
+//                    return ResultData.empty
+//               }
+//               return value
+//           }
+//
+//        let rates = result.map { (data) -> [Rate] in
+//            return data.rates ?? []
+//        }.asDriver(onErrorJustReturn: [])
+//
+//       rates.drive(tableView.rx.items(cellIdentifier: "Cell")) { row, model, cell in
+//            cell.detailTextLabel?.text = model.rate
+//            cell.textLabel?.text = model.code
+//
+//       }.disposed(by: bag)
+//
+//        let title = result.map { (data) -> String in
+//            guard let base = data.base else {
+//                return ""
+//            }
+//            return base
+//        }.asDriver(onErrorJustReturn: "")
+//
+//        title.drive(onNext: { (string) in
+//            self.title = string
+//            }).disposed(by: bag)
+//
+//        let prompt = result.map { (data) -> String in
+//            guard let date = data.date else {
+//                return ""
+//            }
+//            return date
+//        }.asDriver(onErrorJustReturn: "")
+//
+//        prompt.drive(onNext: { (string) in
+//            self.navigationItem.prompt = string
+//        })
+   }
+    
+    private func addCurrencyPickerOnTitle() {
+        Observable.just(allCurrencyCodes)
+        .bind(to: pickerView.rx.itemTitles) { _, code in
+            return code.rawValue
+        }
+        .disposed(by: bag)
+        pickerView.frame = CGRect(x: 0, y: 0, width: 400, height: 80)
+        self.navigationItem.titleView = pickerView
+    }
 }
+
