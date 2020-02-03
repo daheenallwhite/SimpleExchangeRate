afe7b0852bc9c57d788a9fa2c29cfef8214f6a0b
Daheen Lee <allwhite.dev@gmail.com>
2020-02-03 22:25:36 +0900

    Search RIB 추가

---
+++ SimpleExchangeRate/Search/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  SimpleExchangeRate
+//
+//  Created by Daheen Lee on 2020/02/03.
+//  Copyright © 2020 allwhite. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol SearchPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class SearchViewController: UIViewController, SearchPresentable, SearchViewControllable {
+
+    weak var listener: SearchPresentableListener?
+}
